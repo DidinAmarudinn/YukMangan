255560b3f22d12d727866766683a2178a54c7ac1
DidinAmarudinn <amardidin5@gmail.com>
2020-04-04 00:04:02 +0700

    updatepertama

--- app/src/main/java/com/example/yukmangan/DashboardAct.java
+++ app/src/main/java/com/example/yukmangan/DashboardAct.java
@@ -1,14 +1,52 @@
 package com.example.yukmangan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DashboardAct extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class DashboardAct extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bn_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        bn_main=findViewById(R.id.bn_main);
+        loadFragment(new HomeFragment());
+        bn_main.setOnNavigationItemSelectedListener(this);
+    }
+    private boolean loadFragment(Fragment fragment){
+        if (fragment!=null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_container,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment=null;
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment=new HomeFragment();
+                break;
+            case R.id.dahboard_menu:
+
+                break;
+            case R.id.help_menu:
+
+                break;
+
+            case R.id.chat_menu:
+
+                break;
+            case R.id.account_menu:
+
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
