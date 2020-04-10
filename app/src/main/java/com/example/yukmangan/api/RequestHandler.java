449041456082ea5b946f6b4c5ec347ec58d661c8
Dodi Noveli <dodinovely.petanicodng@gmail.com>
2020-04-10 10:33:30 +0700

    requestHandler

--- app/src/main/java/com/example/yukmangan/api/RequestHandler.java
+++ app/src/main/java/com/example/yukmangan/api/RequestHandler.java
@@ -1,4 +1,361 @@
 package com.example.yukmangan.api;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class RequestHandler {
+    Context context;
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public String sendPostRequestUpload(String requestURL, String selectedFilePath) {
+
+        StringBuilder sb = new StringBuilder();
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        final File selectedFile = new File(selectedFilePath);
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length - 1];
+        if (!selectedFile.isFile()) {
+            final String finalSelectedFilePath = selectedFilePath;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d("", "Source File Doesn't Exist:" + finalSelectedFilePath);
+                }
+            }).start();
+
+        } else {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+                URL url = new URL(requestURL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);//Allow Inputs
+                connection.setDoOutput(true);//Allow Outputs
+                connection.setUseCaches(false);//Don't use a cached Copy
+                connection.setReadTimeout(15000);
+                connection.setConnectTimeout(15000);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file", selectedFilePath);
+
+                //creating new dataoutputstream
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+                //writing bytes to data outputstream
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + selectedFilePath + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+                //returns no. of bytes present in fileInputStream
+                bytesAvailable = fileInputStream.available();
+                //selecting the buffer size as minimum of available bytes or 1 MB
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                //setting the buffer as byte array of size of bufferSize
+                buffer = new byte[bufferSize];
+                //reads bytes from FileInputStream(from 0th index of buffer to buffersize)
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                //loop repeats till bytesRead = -1, i.e., no bytes are left to read
+                while (bytesRead > 0) {
+                    //write the bytes read from inputstream
+                    dataOutputStream.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                //closing the input and output streams
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+                /*serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+                Log.i("TAG", "Server Response is: " + serverResponseMessage + ": " + serverResponseCode);
+
+                //response code of 200 indicates the server status OK
+                if(serverResponseCode == 200){
+                    Log.i("TAG", "File Upload completed.\\n\\n You can see the uploaded file here:: " );
+                    *//*runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvFileName.setText("File Upload completed.\n\n You can see the uploaded file here: \n\n" + "http://coderefer.com/extras/uploads/"+ fileName);
+                        }
+                    });*//*
+                }*/
+
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    sb = new StringBuilder();
+                    String response;
+                    //Reading server response
+                    while ((response = br.readLine()) != null) {
+                        sb.append(response);
+                    }
+                } else if (responseCode == HttpsURLConnection.HTTP_NOT_FOUND) {
+                    Toast.makeText(context, "Tidak Terhubung Ke Jaringan", Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+    //Metode Untuk mengirim httpPostRequest
+    //Metode ini mengambil 2 Argumen
+    //Metode Pertama adalah URL dari Skrip yang digunakan untuk mengirimkan permintaan
+    //Yang lainnya adalah HashMap dengan nilai pasangan nama yang berisi data yang akan dikirim dengan permintaan
+    public String sendPostRequest(String requestURL, HashMap<String, String> postDataParams) {
+        //Membuat URL
+        URL url;
+        //Objek StringBuilder untuk menyimpan pesan diambil dari server
+        StringBuilder sb = new StringBuilder();
+        try {
+            url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            //Membuat Keluaran Stream
+            OutputStream os = conn.getOutputStream();
+            //Menulis Parameter Untuk Permintaan
+            //Kita menggunakan metode getPostDataString yang didefinisikan di bawah ini
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                sb = new StringBuilder();
+                String response;
+                //Reading server response
+                while ((response = br.readLine()) != null){
+                    sb.append(response);
+                }
+            }else if (responseCode== HttpsURLConnection.HTTP_NOT_FOUND){
+                Toast.makeText(context, "Tidak Terhubung Ke Jaringan", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return sb.toString();
+    }
+
+    public String sendGetRequest(String requestURL){
+        StringBuilder sb =new StringBuilder();
+        try {
+            URL url = new URL(requestURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            int responseCode = con.getResponseCode();
+
+            if (responseCode== HttpsURLConnection.HTTP_SERVER_ERROR){
+                Toast.makeText(context, "Tidak Terhubung Ke Jaringan", Toast.LENGTH_SHORT).show();
+            }else{
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String s;
+                while((s=bufferedReader.readLine())!=null){
+                    sb.append(s+"\n");
+                }
+            }
+
+        }catch(Exception e){
+            e.getMessage();
+        }
+        return sb.toString();
+    }
+
+    public String sendGetRequestParam(String requestURL, HashMap<String, String> id){
+        StringBuilder sb =new StringBuilder();
+        try {
+            URL url = new URL(requestURL+id);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            /*conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);*/
+            int responseCode = conn.getResponseCode();
+            if (responseCode==HttpsURLConnection.HTTP_SERVER_ERROR){
+                Toast.makeText(context, "Tidak Terhubung Ke Jaringan", Toast.LENGTH_SHORT).show();
+            }else{
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String s;
+                while((s=bufferedReader.readLine())!=null){
+                    sb.append(s+"\n");
+                }
+            }
+
+        }catch(Exception e){
+            e.getMessage();
+        }
+        return sb.toString();
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
+    }
+    public String sendPostRequestEmail(String requestURL, HashMap<String, String> postDataParams,String selectedFilePath) {
+
+        StringBuilder sb = new StringBuilder();
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        final File selectedFile = new File(selectedFilePath);
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length - 1];
+        if (!selectedFile.isFile()) {
+            final String finalSelectedFilePath = selectedFilePath;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d("", "Source File Doesn't Exist:" + finalSelectedFilePath);
+                }
+            }).start();
+
+        } else {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+                URL url = new URL(requestURL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);//Allow Inputs
+                connection.setDoOutput(true);//Allow Outputs
+                connection.setUseCaches(false);//Don't use a cached Copy
+                connection.setReadTimeout(15000);
+                connection.setConnectTimeout(15000);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file", selectedFilePath);
+
+                //creating new dataoutputstream
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+                //writing bytes to data outputstream
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + selectedFilePath + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+                //returns no. of bytes present in fileInputStream
+                bytesAvailable = fileInputStream.available();
+                //selecting the buffer size as minimum of available bytes or 1 MB
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                //setting the buffer as byte array of size of bufferSize
+                buffer = new byte[bufferSize];
+                //reads bytes from FileInputStream(from 0th index of buffer to buffersize)
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                //loop repeats till bytesRead = -1, i.e., no bytes are left to read
+                while (bytesRead > 0) {
+                    //write the bytes read from inputstream
+                    dataOutputStream.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(dataOutputStream, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+
+                //closing the input and output streams
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    sb = new StringBuilder();
+                    String response;
+                    //Reading server response
+                    while ((response = br.readLine()) != null) {
+                        sb.append(response);
+                    }
+                } else if (responseCode == HttpsURLConnection.HTTP_NOT_FOUND) {
+                    Toast.makeText(context, "Tidak Terhubung Ke Jaringan", Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
 }
